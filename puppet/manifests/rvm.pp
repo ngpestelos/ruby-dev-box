b487b6da087e32f58f343a3935dff176c5a30732
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2013-09-05 03:33:49 +0800

    add puppet manifests

---
+++ puppet/manifests/rvm.pp
@@ -0,0 +1,18 @@
+include rvm
+
+if $rvm_installed == "true" {
+  rvm_system_ruby { 'ruby-1.9.3-p448':
+    ensure => 'present',
+    default_use => true;
+  }
+
+  rvm_gem {
+    'ruby-1.9.3-p448@global/puppet':
+    ensure => 'present',
+    require => Rvm_system_ruby['ruby-1.9.3-p448'];
+
+    'ruby-1.9.3-p448@global/bundler':
+    ensure => 'present',
+    require => Rvm_system_ruby['ruby-1.9.3-p448'];
+  }
+}
